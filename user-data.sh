88a8ef768b8e95dfb4cb58582e59c3ca7a21bd89
devops <devops@dev.ops>
2025-01-03 16:42:46 +0900

    change user data

--- user-data.sh
+++ user-data.sh
@@ -6,9 +6,11 @@ mysql -u${db_username} -p${db_password} -h${db_url} ${db_name} < /cloud-demo/cre
 export DB_URL=${db_url}
 export DB_USER=${db_username}
 export DB_PASS=${db_password}
+export DB_NAME=${db_name}
 
 sed -i "s/DB_IP/$DB_URL/g" cloud-demo/process_create.php
-sed -i "s/root/$DB_USER/g" cloud-demo/process_create.php
-sed -i "s/Test123!/$DB_PASS/g" cloud-demo/process_create.php
+sed -i "s/USERNAME/$DB_USER/g" cloud-demo/process_create.php
+sed -i "s/PASSWORD/$DB_PASS/g" cloud-demo/process_create.php
+sed -i "s/DBNAME/$DB_NAME/g" cloud-demo/process_create.php
 
 cp /cloud-demo/*.php /var/www/html
