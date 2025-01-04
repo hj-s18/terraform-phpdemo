18f08c7a8b02794e99c50a5b71b8c2153d084154
HJ <heejnam0918@gmail.com>
2025-01-04 11:43:05 +0900

    Update db.tf

--- db.tf
+++ db.tf
@@ -1,33 +1,12 @@
 resource "aws_db_instance" "phpdb" {
-  identifier_prefix      = "terraform-mysql"
-  engine                 = "mysql"
-  engine_version         = "5.7"
-
-  allocated_storage      = 10
-  instance_class         = "db.t3.micro"
-  skip_final_snapshot    = true
-  vpc_security_group_ids = [aws_security_group.db.id]
-
-  db_name  = var.db_name
-  username = var.db_username
-  password = var.db_password
-}
-
-resource "aws_security_group" "db" {
-  name = "phpdb-sg"
-
-  ingress {
-    from_port       = 3306
-    to_port         = 3306
-    protocol        = "tcp"
-    security_groups = [aws_security_group.instance.id]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  identifier_prefix   = "terraform-mysql"
+  engine              = "mysql"
+  allocated_storage   = 10
+  instance_class      = "db.t3.micro"
+  skip_final_snapshot = true
+  db_name             = var.db_name
+  username            = var.db_username
+  password            = var.db_password
 }
 
 variable "db_name" {
@@ -61,8 +40,8 @@ output "port" {
   description = "The port the database is listening on"
 }
 
+# address:port = endpoint
 output "endpoint" {
   value       = aws_db_instance.phpdb.endpoint
   description = "The endpoint of database"
 }
-# address:port -> endpoint
