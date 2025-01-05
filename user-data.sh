c9e03e4f54e78cb1ead094d634db7356b1e110f4
HJ <heejnam0918@gmail.com>
2025-01-05 15:55:47 +0900

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -4,12 +4,11 @@ git clone -b gitclone https://github.com/hj-s18/phpdemo.git
 mysql -u${db_username} -p${db_password} -h${db_url} ${db_name} < /phpdemo/create_table_items.sql
 
 export DB_URL=${db_url}
-sed -i "s/DB_IP/$DB_URL/g" /phpdemo/process_create.php
-
 export DB_USER=${db_username}
-sed -i "s/root/$DB_USER/g" /phpdemo/process_create.php
-
 export DB_PASS=${db_password}
+
+sed -i "s/DB_IP/$DB_URL/g" /phpdemo/process_create.php
+sed -i "s/root/$DB_USER/g" /phpdemo/process_create.php
 sed -i "s/Test123!/$DB_PASS/g" /phpdemo/process_create.php
 
 cp /phpdemo/*.php /var/www/html
