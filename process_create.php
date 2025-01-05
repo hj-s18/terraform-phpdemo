65267455e9cce6e854ed8ce00fd410c26de4bce2
HJ <heejnam0918@gmail.com>
2025-01-05 15:10:25 +0900

    Create process_create.php

---
+++ process_create.php
@@ -0,0 +1,14 @@
+<?php
+
+$conn = mysqli_connect("DB_IP","USERNAME","PASSWORD","DBNAME",3306);
+
+$sql = "insert into items (title, description, created) value ('{$_POST['title']}','{$_POST['description']}', now())";
+
+mysqli_query($conn,$sql);
+if ($result=== false){
+    echo 'error occured.';
+    error_log(mysqli_error($conn));
+}
+echo 'Succeed. <a href="index.php"> back</a>';
+
+?>
