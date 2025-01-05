cb1e16cc21a3bacb0e4035423f987ecb14174b46
HJ <heejnam0918@gmail.com>
2025-01-05 13:24:02 +0900

    Create webserver.sh

---
+++ webserver.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+curl -O http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+sudo rpm -Uvh remi-release-7.rpm
+
+sudo yum install -y yum-utils
+sudo yum-config-manager --enable remi-php72
+sudo yum install -y php php-common php-mysqli
+
+sudo systemctl start httpd
+sudo systemctl enable httpd
