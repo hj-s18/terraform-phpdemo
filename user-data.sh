c89c779c674195af6b607692fca198a99d434aa8
HJ <heejnam0918@gmail.com>
2025-01-05 15:58:43 +0900

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -6,9 +6,11 @@ mysql -u${db_username} -p${db_password} -h${db_url} ${db_name} < /phpdemo/create
 export DB_URL=${db_url}
 export DB_USER=${db_username}
 export DB_PASS=${db_password}
+export DB_NAME=${db_name}
 
 sed -i "s/DB_IP/$DB_URL/g" /phpdemo/process_create.php
-sed -i "s/root/$DB_USER/g" /phpdemo/process_create.php
-sed -i "s/Test123!/$DB_PASS/g" /phpdemo/process_create.php
+sed -i "s/USERNAME/$DB_USER/g" /phpdemo/process_create.php
+sed -i "s/PASSWORD/$DB_PASS/g" /phpdemo/process_create.php
+sed -i "s/DBNAME/$DB_NAME/g" / phpdemo/process_create.php
 
 cp /phpdemo/*.php /var/www/html
