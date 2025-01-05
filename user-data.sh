3c1cd4679067942ecbcf7f5df64ad269015ff64b
HJ <heejnam0918@gmail.com>
2025-01-05 12:04:20 +0900

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -3,7 +3,7 @@
 
 #!/bin/bash                                                                                         # 셸 인터프리터 선언 : 이 스크립트가 Bash셸에서 실행된다는 것을 지정
 git clone -b create-table https://github.com/hj-s18/phpdemo.git                                     # git 저장소 클론(복제) : 저장소가 현재 디렉토리 안에 phpdemo 폴더로 복제됨 (create-table 브랜치에 포함된 파일만 가져옴)
-mysql -u${db_username} -p${db_password} -h${db_url} ${db_name} < /phpdemo/create_db_webtest.sql     # MySQL 서버에 연결하고, SQL 파일을 실행하여 데이터베이스 초기화 작업 수행하거나 테이블 등을 생성함 (테이블 생성하는 코드 들어있음)
+mysql -u${db_username} -p${db_password} -h${db_url} ${db_name} < /phpdemo/create_table_items.sql    # MySQL 서버에 연결하고, SQL 파일을 실행하여 데이터베이스 초기화 작업 수행하거나 테이블 등을 생성함 (테이블 생성하는 코드 들어있음)
                                                                                                       # -u${db_username} : MySQL 데이터베이스 사용자 이름 ⇒ Terraform에서 제공된 변수값(var.db_username)이 ${db_username}에 치환됨
                                                                                                       # -p${db_password} : MySQL 데이터베이스 비밀번호 ⇒ Terraform에서 제공된 변수값(var.db_password)이 ${db_password}에 치환됨
                                                                                                       # -h${db_url} : 데이터베이스 서버 주소(hostname) : RDS 인스턴스의 주소(aws_db_instance.phpdb.address)가 ${db_url}에 치환됨
