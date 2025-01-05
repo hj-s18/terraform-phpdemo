940d3785252e08e21d77c612fe906e511ec72dea
HJ <heejnam0918@gmail.com>
2025-01-05 16:51:54 +0900

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,1 +1,4 @@
-provider "aws" {}
+provider "aws" {}    # AWS 클라우드와 테라폼 연결
+                     # provider "aws" : terraform이 AWS 클라우드와 상호작용하도록 설정
+                       # {} : 별도의 옵션 없이 기본 설정으로 AWS 사용
+                       # AWS 지역 설정은 ~/.aws/credentials 또는 환경변수로 제공됨
