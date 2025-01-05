36293fd1632ed64b794ee7e4bed8a5dc445e7288
HJ <heejnam0918@gmail.com>
2025-01-05 18:03:57 +0900

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-git clone -b gitclone https://github.com/hj-s18/terraform-practice.git
-mysql -u${db_username} -p${db_password} -h${db_url} ${db_name} < /terraform-practice/create_table_items.sql
+git clone -b gitclone https://github.com/hj-s18/terraform-phpdemo.git
+mysql -u${db_username} -p${db_password} -h${db_url} ${db_name} < /terraform-phpdemo/create_table_items.sql
