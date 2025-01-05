b79f10e16c44d491aa5c6b29a9e3d68a245ef346
HJ <heejnam0918@gmail.com>
2025-01-05 14:43:55 +0900

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-git clone -b create-table https://github.com/hj-s18/phpdemo.git
+git clone -b gitclone https://github.com/hj-s18/phpdemo.git
 mysql -u${db_username} -p${db_password} -h${db_url} ${db_name} < /phpdemo/create_table_items.sql
