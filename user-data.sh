3471943c3d3439bbbbebe81d689970dc97596cd3
HJ <heejnam0918@gmail.com>
2025-01-05 18:01:02 +0900

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-git clone -b gitclone https://github.com/hj-s18/phpdemo.git
-mysql -u${db_username} -p${db_password} -h${db_url} ${db_name} < /phpdemo/create_table_items.sql
+git clone -b gitclone https://github.com/hj-s18/terraform-phpdemo.git
+mysql -u${db_username} -p${db_password} -h${db_url} ${db_name} < /terraform-phpdemo/create_table_items.sql
 
 export DB_URL=${db_url}
 export DB_USER=${db_username}
 export DB_PASS=${db_password}
 export DB_NAME=${db_name}
 
-sed -i "s/DB_IP/$DB_URL/g" /phpdemo/process_create.php
-sed -i "s/USERNAME/$DB_USER/g" /phpdemo/process_create.php
-sed -i "s/PASSWORD/$DB_PASS/g" /phpdemo/process_create.php
-sed -i "s/DBNAME/$DB_NAME/g" / phpdemo/process_create.php
+sed -i "s/DB_IP/$DB_URL/g" /terraform-phpdemo/process_create.php
+sed -i "s/USERNAME/$DB_USER/g" /terraform-phpdemo/process_create.php
+sed -i "s/PASSWORD/$DB_PASS/g" /terraform-phpdemo/process_create.php
+sed -i "s/DBNAME/$DB_NAME/g" /terraform-phpdemo/process_create.php
 
-cp /phpdemo/*.php /var/www/html
+cp /terraform-phpdemo/*.php /var/www/html
